d5ad481779b4736da65364c0b35c7499834fbbf4
Gilbert Clark <clarkg1@ohio.edu>
2014-07-15 19:31:07 -0400

    A few tweaks to catch keyboard interrupts / EOF

--- util/pcap-fetch.py
+++ util/pcap-fetch.py
@@ -16,11 +16,19 @@ pcaplist = [PcapSource('http://www.bro.org/static/traces/2009-M57-day11-18.trace
             PcapSource('http://2009.hack.lu/archive/2009/InfoVisContest/jubrowska-capture_1.cap', 'Information Security Visualization Contest - hack.lu 2009')]
 
 class PcapFetchPrompt(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+
     def do_list(self, arg):
         """List of available packet captures"""
         for capture in pcaplist:
             print "%s - %s  [%s]" % (capture.name, capture.description, capture.url)
 
+    def do_EOF(self, arg):
+        print ""
+        self.do_exit(arg)
+
+    # Credit: http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
     def do_fetch(self, arg):
         """Fetches a particular packet capture"""
         target = None
@@ -33,32 +41,37 @@ class PcapFetchPrompt(cmd.Cmd):
             print "No such capture: %s" % arg
             return
 
-        file_name = target.split('/')[-1]
-        u = urllib2.urlopen(target)
-        f = open(file_name, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Saving to: %s   [%s MB]" % (file_name, int(100 * file_size / 1024.0 / 1024.0) / 100.0)
+        try:
+            file_name = target.split('/')[-1]
+            u = urllib2.urlopen(target)
+            f = open(file_name, 'wb')
+            meta = u.info()
+            file_size = int(meta.getheaders("Content-Length")[0])
+            print "Saving to: %s   [%s MB]" % (file_name, int(100 * file_size / 1024.0 / 1024.0) / 100.0)
 
-        file_size_dl = 0
-        block_sz = 65536
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
+            file_size_dl = 0
+            block_sz = 65536
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
 
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = "\rRetrieved: %10dM  [%3.2f%%]\r" % (file_size_dl / 1024 / 1024, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            sys.stdout.write(status)
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status = "\rRetrieved: %10dM  [%3.2f%%]\r" % (file_size_dl / 1024 / 1024, file_size_dl * 100. / file_size)
+                status = status + chr(8)*(len(status)+1)
+                sys.stdout.write(status)
 
-        f.close()
-        print
+            f.close()
+            print ""
+            print "Fetch completed!"
+        except KeyboardInterrupt:
+            print ""
+            print "Fetch aborted!"
 
     def do_quit(self, arg):
         """Exits this prompt"""
-        sys.exit(0)
+        self.do_exit(arg)
 
     def do_exit(self, arg):
         """Exits this prompt"""
@@ -67,4 +80,10 @@ class PcapFetchPrompt(cmd.Cmd):
 if __name__ == '__main__':
     prompt = PcapFetchPrompt()
     prompt.prompt = 'pcap-fetch> '
-    prompt.cmdloop('List all available captures by entering "list", or fetch a particular capture by using "fetch"')
+    while True:
+        try:
+            prompt.cmdloop('List all available captures by entering "list", or fetch a particular capture by using "fetch"')
+        except KeyboardInterrupt:
+            print ""
+            print "Exiting on CTRL-C..."
+            sys.exit(0)
